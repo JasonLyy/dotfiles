a8ac5b2b84e27c915597e3a6200cd951ee8ba62f
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2023-03-30 15:22:10 +1100

    feat: oh-my-zsh install

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
-
 echo "1. Setup Brew"
-# echo -ne | eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo -ne | eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew doctor
-# brew update
+brew doctor
+brew update
+
+echo "2. Setup oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # TODO backup old files
-echo "2. Remove old dotfiles"
+echo "3. Remove old dotfiles"
 rm -rf ~/.zshrc
 rm -r ~/.gitconfig
 
-echo "3. Setup symlinks"
+echo "4. Setup symlinks"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+zsh
