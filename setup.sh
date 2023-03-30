970ea1c1fc5a3598c8eaf3c20211272b213662b1
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2023-03-30 14:53:56 +1100

    feat: base symlink git and zsh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-#1. Setup Brew
-echo -ne | eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "1. Setup Brew"
+# echo -ne | eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew doctor
-brew update
+# brew doctor
+# brew update
+
+# TODO backup old files
+echo "2. Remove old dotfiles"
+rm -rf ~/.zshrc
+rm -r ~/.gitconfig
+
+echo "3. Setup symlinks"
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
