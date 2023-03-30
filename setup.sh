962e5e5a9ebc48f040a2aad012dc42959e2a5233
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2023-03-30 21:57:28 +1100

    feat: install p10k fonts and iterm

--- setup.sh
+++ setup.sh
@@ -4,15 +4,37 @@ echo -ne | eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install
 brew doctor
 brew update
 
+echo "2. Install Brew packages"
+brew install --cask iterm2
+
 echo "2. Setup oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+echo "3. Setup p10k fonts for oh-my-zsh"
+fontsLink=(
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+)
+fontsName=(
+    "MesloLGS NF Regular.ttf"
+    "MesloLGS NF Bold.ttf"
+    "MesloLGS NF Italic.ttf"
+    "MesloLGS NF Bold Italic.ttf"
+)
+fontsLen=${#fontsName[@]}
+
+for i in "${!fontsLink[@]}"; do
+    (cd ~/Library/Fonts/ && curl -L -o "${fontsName[i]}" ${fontsLink[i]})
+done
+
 # TODO backup old files
-echo "3. Remove old dotfiles"
+echo "4. Remove old dotfiles"
 rm -rf ~/.zshrc
 rm -r ~/.gitconfig
 
-echo "4. Setup symlinks"
+echo "5. Setup symlinks"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
