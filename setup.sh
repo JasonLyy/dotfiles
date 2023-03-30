87d9a38473bd7c16904871f02d362cc1c605f0d5
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2023-03-31 10:45:08 +1100

    feat: use brew to install fonts

--- setup.sh
+++ setup.sh
@@ -10,24 +10,10 @@ brew install --cask iterm2
 echo "3. Setup oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo "4. Setup p10k fonts for oh-my-zsh"
-fontsLink=(
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
-    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
-)
-fontsName=(
-    "MesloLGS NF Regular.ttf"
-    "MesloLGS NF Bold.ttf"
-    "MesloLGS NF Italic.ttf"
-    "MesloLGS NF Bold Italic.ttf"
-)
-fontsLen=${#fontsName[@]}
-
-for i in "${!fontsLink[@]}"; do
-    (cd ~/Library/Fonts/ && curl -L -o "${fontsName[i]}" ${fontsLink[i]})
-done
+echo "4. Setup fonts"
+brew tap homebrew/cask-fonts
+brew install font-fira-code
+brew install font-meslo-lg
 
 echo "5. Setup p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
