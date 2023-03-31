64398619d6caf31ac85b5eea85bda9d0eef52e3b
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2023-03-31 11:19:03 +1100

    feat: zsh uses antigen plugin manager

--- setup.sh
+++ setup.sh
@@ -7,25 +7,21 @@ brew update
 echo "2. Install Brew packages"
 brew install --cask iterm2
 
-echo "3. Setup oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-echo "4. Setup fonts"
 brew tap homebrew/cask-fonts
 brew install font-fira-code
 brew install font-meslo-lg
 
-echo "5. Setup p10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "3. Setup zsh antigen"
+git clone https://github.com/zsh-users/antigen.git ~/antigen
 
 # TODO backup old files
-echo "5. Remove old dotfiles"
+echo "4. Remove old dotfiles"
 rm -rf ~/.zshrc
 rm -rf ~/.gitconfig
 rm -rf ~/.p10k.zsh
 rm -rf ~/Library/Application\ Support/Code/User/settings.json
 
-echo "6. Setup symlinks"
+echo "5. Setup symlinks"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
